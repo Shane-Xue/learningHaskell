a697a544db080c4875019d8496bcc029f99060a4
unknown <qq2198631773@outlook.com>
2020-12-06 19:40:32 +0800

    My first runnable Haskell program On how much I love Treasure dear (heart)

--- restart.hs
+++ restart.hs
@@ -1,6 +1,7 @@
 
-
+circleArea :: Float -> Float
 circleArea x = pi * x * x
 
+squareArea :: Float -> Float
 squareArea x = x * x
 
--- treasure.hs
+++ treasure.hs
@@ -1,7 +1,15 @@
 -- A strange try to represent Treasure dear in haskell
-module Treasure where
+--module Treasure where
 
+tsrName :: String
 tsrName = "Treasure"
 
+tsrAge :: Integer
 tsrAge = 14
 
+main :: IO()
+main = do
+       putStrLn("Treasure dear!")
+       putStr("You're already ")
+       print(tsrAge)
+       putStrLn("Love you forever!~~")
